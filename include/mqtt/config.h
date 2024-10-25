0db29f517b240f06d009370e48999d477499e2a5
Adam Brown <108494346+AdamBrown0@users.noreply.github.com>
2024-10-25 03:18:57 +0100

    Create config.h

---
+++ include/mqtt/config.h
@@ -0,0 +1,14 @@
+#ifndef config_h
+#define config_h
+
+#include <Arduino.h>
+
+// WiFi
+#define WIFI_SSID       "<your_ssid>"
+#define WIFI_PASSWORD   "<your_password>"
+
+// MQTT
+#define MQTT_HOST       "<broker ip address>"
+#define MQTT_PORT       <broker port> // usually 1883
+
+#endif
